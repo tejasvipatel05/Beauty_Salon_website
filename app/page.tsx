6e1fb1a60709184291dba5ba7b15ed227a969b09
tejasvi-patel <tejasvipatel1010@gmail.com>
2025-07-23 22:47:05 +0530

    Error solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 'use client';
-import Link from "next/link";
-import homestyles from '../styles/Home.module.css';
 import { useTheme } from '@/context/ThemeContext';
 import landingstyles from '@/styles/Landing.module.css';
 
 export default function Home() {
-  const { theme, toggleTheme } = useTheme();
-
   return (
     <div className={landingstyles.container}>
         <section className={landingstyles.hero}>
